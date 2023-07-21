82fbff38ad72f33e5faa6e14b8e9e764eb67d7aa
Geeth Gunnampalli <geeth.gunnampalli@gmail.com>
2023-07-21 16:58:41 -0500

    initual commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,182 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
+import { MenuItem } from 'primeng/api';
+import { ActivatedRoute } from '@angular/router';
+
+export interface Investigator {
+  status: boolean;
+  step: string;
+  notes: string;
+}
+
+export interface TriageIncident {
+  id: number;
+  incidentName: string;
+  siteName: string;
+  incidentSummary: string;
+  incidentDetails: string;
+  status: Codes;
+  assignedTo: Users;
+  reportedBy: Users;
+  incidentType: Codes;
+  incidentDate: Date;
+  updatedBy: string;
+  updatedDate: Date;
+  associateAlarmIds: number[];
+}
+
+export interface Users {
+  id: number;
+  name: string;
+}
+
+export interface Codes {
+  id: number;
+  name: string;
+}
+
+let ELEMENT_DATA: Investigator[] = [
+  {
+    status: true,
+    step: 'Power Loss: The battery energy storage plant is currently offline, with no energy output being generated.',
+    notes: '',
+  },
+  {
+    status: false,
+    step: 'Power Loss: The battery energy storage plant is currently offline, with no energy output being generated.',
+    notes: '',
+  },
+  {
+    status: false,
+    step: 'Overheating Warning: The temperature of the battery system has exceeded safe operating limits.',
+    notes: '',
+  },
+  {
+    status: false,
+    step: 'Power Restoration: The battery energy storage plant has been successfully brought back online and is now generating energy.',
+    notes: '',
+  },
+  {
+    status: false,
+    step: 'Critical Alert: Multiple battery cells have failed, causing a significant reduction in energy output.',
+    notes: '',
+  },
+  {
+    status: false,
+    step: 'Routine Maintenance: The battery energy storage plant is undergoing scheduled maintenance for optimal performance.',
+    notes: '',
+  },
+];
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [
+    {
+      provide: STEPPER_GLOBAL_OPTIONS,
+      useValue: { showError: false },
+    },
+  ],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  items: MenuItem[] | undefined;
+
   title = 'ims';
+  displayedColumns: string[] = ['position', 'step', 'weight'];
+  dataSource = ELEMENT_DATA;
+  activeIndex: number = 0;
+
+  constructor(private route: ActivatedRoute) {} // Inject the ActivatedRoute in the constructor
+
+  onActiveIndexChange(event: number) {
+    this.activeIndex = event;
+  }
+  checked: boolean = false;
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Incident Details',
+      },
+      {
+        label: 'Investigation',
+      },
+      {
+        label: 'Fixing',
+      },
+      {
+        label: 'Fixed',
+      },
+    ];
+  }
+
+  triageIncident: TriageIncident = {
+    id: 1,
+    incidentName: 'Battery Energy Storage Plant',
+    siteName: 'Brisbane',
+    incidentSummary: 'Power Loss',
+    incidentDetails:
+      'The battery energy storage plant is currently offline, with no energy output being generated.',
+    status: {
+      id: 1,
+      name: 'Reported',
+    },
+    assignedTo: {
+      id: 1,
+      name: 'John Smith',
+    },
+    reportedBy: {
+      id: 1,
+      name: 'John Smith',
+    },
+    incidentType: {
+      id: 1,
+      name: 'Power Loss',
+    },
+    incidentDate: new Date(),
+    updatedBy: 'John Smith',
+    updatedDate: new Date(),
+    associateAlarmIds: [1, 2, 3],
+  };
+
+  statusCodes: Codes[] = [
+    {
+      id: 1,
+      name: 'Reported',
+    },
+    {
+      id: 2,
+      name: 'Investigating',
+    },
+    {
+      id: 3,
+      name: 'Fixing',
+    },
+    {
+      id: 4,
+      name: 'Fixed',
+    },
+  ];
+
+  users: Users[] = [
+    {
+      id: 1,
+      name: 'John Smith',
+    },
+    {
+      id: 2,
+      name: 'Jane Doe',
+    },
+    {
+      id: 3,
+      name: 'John Doe',
+    },
+    {
+      id: 4,
+
+      name: 'Jane Smith',
+    },
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { TextFieldModule } from '@angular/cdk/text-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 
+import { RouterModule, Routes } from '@angular/router';
+import { CheckboxModule } from 'primeng/checkbox';
+import { StepsModule } from 'primeng/steps';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { SelectButtonModule } from 'primeng/selectbutton';
+import { DropdownModule } from 'primeng/dropdown';
+const routes: Routes = [
+  // Define your routes here...
+];
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    TextFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CheckboxModule,
+    StepsModule,
+    RouterModule.forRoot(routes), // Add this line to configure the router module
+    ButtonModule,
+    InputTextModule,
+    SelectButtonModule,
+    DropdownModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
